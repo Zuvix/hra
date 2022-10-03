1effa53a7d91728a9f1df1115384e6122597b554
Zuvix <samuelkubala@gmail.com>
2022-10-03 11:45:51 +0200

    Bowling

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(1, 0, 0));
+        }
+
+    }
+}
